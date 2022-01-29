6711c5d6426bd90c464c8b88edd73face0a6dc19
Ziling Yang <zilyang@bu.edu>
2022-01-29 15:04:05 -0500

    Commit

--- CountingSort.py
+++ CountingSort.py
@@ -7,8 +7,8 @@ def CountingSort(A,k):
     # Count elements in A. C[i] stores how many element i are in A
     for i in range(len(A)):
         C[A[i]] = C[A[i]]+1 
-    # Count elements less than or equal to i. C[i] = #elements = to or < than i    
-    for i in range(1,k):
+    # Count elements less than or equal to i. C[i] = #elements = to or < than i
+    for i in range(1,k+1):
         C[i] = C[i] + C[i-1]
     # Initialize return array
     B = [None]*len(A)
@@ -20,7 +20,8 @@ def CountingSort(A,k):
 
         
 def main():
-     assert CountingSort([2,5,3,0,2,3,0,3],5)!=[0,0,2,2,3,3,3,5]
+     assert CountingSort([2,5,3,0,2,3,0,3],5)==[0,0,2,2,3,3,3,5], ("Test case failed")
+     print("complete")
      
 
 if __name__ == "__main__":
